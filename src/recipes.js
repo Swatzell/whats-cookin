4ec1199b3e595e7fb32f6cd530884594079e0a73
Zach Wolek <zach.wolek@gmail.com>
2024-03-29 20:57:29 -0500

    Create a functioning Tag search and Home button

--- src/recipes.js
+++ src/recipes.js
@@ -50,9 +50,5 @@ export const calculateRecipeCost = (recipeID, recipeData, ingredientsData) => {
 
 export const getRecipeInstructions = (recipeID, recipeData) => {
   const recipe = recipeData.find((recipe) => recipe.id === recipeID);
-  return recipe.instructions.map((instruction) => `${instruction.number}: ${instruction.instruction}`);;
+  return recipe.instructions.map((instruction) => `${instruction.number}: ${instruction.instruction}`);
 };
-
-
-
-
